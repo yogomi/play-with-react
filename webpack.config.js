681011ad34aeb482ad1aef4a874139c6dfa213a0
Makoto Yano <yan133@gmail.com>
2017-11-28 16:16:31 +0900

    reactをビルドできるようにまでした。

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,25 @@
 module.exports = {
-  entry: "./src/main.js",
+  entry: "./src/main.jsx",
   output: {
     filename: "build/bundle.js"
-  }
+  },
+  module: {
+   rules: [
+     {
+       test: /\.js[x]?$/,
+       loader: "babel-loader",
+       exclude: /node_modules/,
+       options: {
+         presets: [
+           ["env", {"modules": false}],
+           "react",
+         ]
+       }
+     },
+    ],
+  },
+  resolve: {
+    modules: [__dirname, "node_modules"],
+    extensions: [".js", ".jsx"],
+  },
 }
